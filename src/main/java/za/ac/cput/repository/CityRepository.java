5ee5517cb4833aa36d0ab7ca2c67d51ce43d009d
Ali <alimohamedreddit@gmail.com>
2022-06-18 15:58:28 +0200

    fixed application.yaml file

--- src/main/java/za/ac/cput/repository/CityRepository.java
+++ src/main/java/za/ac/cput/repository/CityRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.City;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, String> {
+    List<City> findCitiesByCountry_Id(String id);
 }
